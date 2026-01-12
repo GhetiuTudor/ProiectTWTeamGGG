cf1495d81db87723abf9ce1ff18378e728f919a0
GXT <tudorghetiu1@gmail.com>
2026-01-12 17:48:29 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 
-// Protected Route Wrapper
+
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth();
 
