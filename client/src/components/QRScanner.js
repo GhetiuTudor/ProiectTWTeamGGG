a152fe34f42b2f5a1bbe4f85feb0d560dadb2e86
GXT <tudorghetiu1@gmail.com>
2026-01-12 17:42:50 +0200

    Update QRScanner.js

--- client/src/components/QRScanner.js
+++ client/src/components/QRScanner.js
@@ -8,25 +8,25 @@ const QRScanner = ({ onScan, onClose }) => {
         const scanner = new Html5QrcodeScanner(
             "reader",
             { fps: 10, qrbox: { width: 250, height: 250 } },
-      /* verbose= */ false
+       false
         );
 
         scanner.render(onScanSuccess, onScanFailure);
 
         function onScanSuccess(decodedText) {
-            // Handle the scanned code as you like, for example:
+            
             setScanResult(decodedText);
             scanner.clear();
             onScan(decodedText);
         }
 
         function onScanFailure(error) {
-            // handle scan failure, usually better to ignore and keep scanning.
+          
         }
 
         return () => {
             scanner.clear().catch(error => {
-                // Failed to clear scanner. 
+               
                 console.error("Failed to clear html5-qrcode scanner. ", error);
             });
         };
